356afbf58dd0992ac83f11222d6ea7b7a7e7150a
dhairyalakhmani <dhairyalakhmani27@gmail.com>
2026-04-02 11:59:45 +0530

    Created nextPointerInBinaryTree.java

---
+++ BinaryTree/nextPointerInBinaryTree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return root;
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                Node temp = q.poll();
+                if (i < size - 1) {
+                    temp.next = q.peek();
+                } else {
+                    temp.next = null;
+                }
+                if (temp.left != null) q.add(temp.left);
+                if (temp.right != null) q.add(temp.right);
+            }
+        }
+        return root;
+    }
+}
