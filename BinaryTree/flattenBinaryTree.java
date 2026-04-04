0806ea94e219c77544932bab0dd040bbff758fbf
dhairyalakhmani <dhairyalakhmani27@gmail.com>
2026-04-04 11:27:44 +0530

    Created flattenBinaryTree.java

---
+++ BinaryTree/flattenBinaryTree.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void flatten(TreeNode root) {
+        if(root == null) return;
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = null;
+        TreeNode curr = root;
+        while(curr.right != null){
+            curr = curr.right;
+        }
+        curr.right = temp;   
+    }
+}
